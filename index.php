8be1c4fab5d932a9c046331c8a6fce70ad73cc89
LEMUEL CAMPOS MENEZES GOMES <lemuel0072004@gmail.com>
2023-07-05 21:31:22 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Logo INSTAGRAM</title>
+</head>
+<body>
+    <div id="logo-instagram">
+        
+        <div id= "logo-instagram-borda"></div>
+            <div id= "logo-instagram-circulo"></div>
+                <div id= "logo-instagram-ponto"></div>
+
+    </div>
+</body>
+</html>
